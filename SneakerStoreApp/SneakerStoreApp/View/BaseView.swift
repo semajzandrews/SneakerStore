0347c725a14f8b533e434d97386aa18d8081d70e
semajzandrews <semajzandrews@gmail.com>
2022-07-21 16:13:30 -0400

    Added Cart Button onto BaseView

--- SneakerStoreApp/SneakerStoreApp/View/BaseView.swift
+++ SneakerStoreApp/SneakerStoreApp/View/BaseView.swift
@@ -20,7 +20,7 @@ struct BaseView: View {
         TabView(selection: $baseData.currentTab){
             Text("Home")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.black.opacity(0.4))
+                .background(Color.black.opacity(0.0))
                 .tag(Tab.Home)
             Text("Heart")
                 .tag(Tab.Heart)
@@ -34,11 +34,31 @@ struct BaseView: View {
             HStack(spacing: 0){
                 TabButton(Tab: .Home)
                 TabButton(Tab: .Heart)
+                
+                // MARK: CENTERED CIRCULARD CART BUTTON
+                
+                Button {
+                    
+                } label: {
+                    Image("cart")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 26, height: 26)
+                        .foregroundColor(.white)
+                        .offset(x: -1)
+                        .padding(18)
+                        .background(Color("DarkBlue"))
+                        .clipShape(Circle())
+                }
+                .offset(y: -24)
+                
                 TabButton(Tab: .Clipboard)
                 TabButton(Tab: .Person)
-            },
+            }
+                .background(Color.white)
             
-            alignment: .bottom
+            ,alignment: .bottom
         )
     }
     
