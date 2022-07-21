177badb56ee33fbe4ce6e189ae30da53b28334e5
semajzandrews <semajzandrews@gmail.com>
2022-07-21 15:51:59 -0400

    Added TabView Image Assets

--- SneakerStoreApp/SneakerStoreApp/View/BaseView.swift
+++ SneakerStoreApp/SneakerStoreApp/View/BaseView.swift
@@ -11,13 +11,47 @@ struct BaseView: View {
     
     @StateObject var baseData = BaseViewModel()
     
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
         
         TabView(selection: $baseData.currentTab){
-            
+            Text("Home")
+                .tag(Tab.Home)
+            Text("Heart")
+                .tag(Tab.Heart)
+            Text("Clipboard")
+                .tag(Tab.Clipboard)
+            Text("Person")
+                .tag(Tab.Person)
         }
-        
+        .overlay(
+            // MARK: CUSTOM TAB BAR
+            HStack(spacing: 0){
+                
+            }
+        )
     }
+    
+    @ViewBuilder
+    func TabButton(Tab: Tab) -> some View {
+        
+        Button {
+            withAnimation {
+                baseData.currentTab = Tab
+            }
+        } label: {
+            Image(Tab.rawValue)
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+//                .foregroundColor(<#T##color: Color?##Color?#>)
+        }
+    }
+    
 }
 
 struct BaseView_Previews: PreviewProvider {
