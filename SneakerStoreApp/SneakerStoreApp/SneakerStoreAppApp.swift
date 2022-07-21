edeb785f26834fdc0f2fb1c28fd0a4e7a8b68ff6
semajzandrews <semajzandrews@gmail.com>
2022-07-21 15:18:33 -0400

    Adding Project Starter Files

---
+++ SneakerStoreApp/SneakerStoreApp/SneakerStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerStoreAppApp.swift
+//  SneakerStoreApp
+//
+//  Created by Semaj Andrews on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SneakerStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
